b736a1bb8925088c86e388213b6206a9b6f42586
Kolejarzyk <pawel.dee@live.com>
2018-11-12 15:26:06 +0100

    Simple layout for Login and Register Page

--- app/src/main/java/com/example/kolejarz/cook/HomePage.kt
+++ app/src/main/java/com/example/kolejarz/cook/HomePage.kt
@@ -1,12 +1,28 @@
 package com.example.kolejarz.cook
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home_page.*
 
 class HomePage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+
+        sing_in_button_home_page.setOnClickListener{
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
+        register_button_home_page.setOnClickListener{
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
     }
+
 }
+
+
---
+++ app/src/main/java/com/example/kolejarz/cook/LoginPage.kt
@@ -0,0 +1,13 @@
+package com.example.kolejarz.cook
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class LoginPage : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
