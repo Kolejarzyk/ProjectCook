2813b7fa6e39190f87c10d131e59894b973d186a
hentaininja <hasamt94@gmail.com>
2018-11-21 14:01:35 +0100

    side_panel.kt and activity_sidepanel_page changes

---
+++ app/src/main/java/com/example/kolejarz/cook/SidePanelPage.kt
@@ -0,0 +1,38 @@
+package com.example.kolejarz.cook
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sidepanel_page.*
+
+class SidePanelPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sidepanel_page)
+
+
+        create_recipe_button_side_panel.setOnClickListener{
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
+        my_builds_button_side_panel.setOnClickListener{
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
+        favourites_button_side_panel.setOnClickListener{
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
+        ranking_button_side_panel.setOnClickListener{
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
+
+        products_list_button_side_panel.setOnClickListener{
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
