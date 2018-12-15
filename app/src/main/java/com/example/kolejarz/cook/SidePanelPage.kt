ea33a32a186bf4d08e21336a98362202e1b15628
hentaininja <hasamt94@gmail.com>
2018-12-15 18:22:07 +0100

    just WORK ALREADY!

--- app/src/main/java/com/example/kolejarz/cook/SidePanelPage.kt
+++ app/src/main/java/com/example/kolejarz/cook/SidePanelPage.kt
@@ -1,71 +1,71 @@
 package com.example.kolejarz.cook
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sidepanel.*
 
 class SidePanelPage : AppCompatActivity() {
-
-    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sidepanel)
 
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        // Configure action bar
         setSupportActionBar(toolbar)
-        val actionbar: ActionBar? = supportActionBar
-        actionbar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
+        val actionBar = supportActionBar
+        actionBar?.title = "Hello Toolbar"
+
+
+        // Initialize the action bar drawer toggle instance
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ){
+            override fun onDrawerClosed(view: View){
+                super.onDrawerClosed(view)
+                //toast("Drawer closed")
+            }
+
+            override fun onDrawerOpened(drawerView: View){
+                super.onDrawerOpened(drawerView)
+                //toast("Drawer opened")
+            }
         }
 
-        mDrawerLayout = findViewById(R.id.drawer_layout)
 
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // set item as selected to persist highlight
-            menuItem.isChecked = true
-            // close drawer when item is tapped
-            mDrawerLayout.closeDrawers()
-            // Add code here to update the UI based on the item selected
-            // For example, swap UI fragments here
+        // Configure the drawer layout to add listener and show icon on toolbar
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+
+        // Set navigation view navigation item selected listener
+        navigation_view.setNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.create_recipe_button_side_panel -> toast("Create Recipe clicked")
+                R.id.my_builds_button_side_panel -> toast("My builds clicked")
+                R.id.favourites_button_side_panel -> toast("Favourites clicked")
+                R.id.products_list_button_side_panel -> toast("Products list clicked")
+            }
+            // Close the drawer
+            drawer_layout.closeDrawer(GravityCompat.START)
             true
         }
-
-        /*mDrawerLayout.addDrawerListener(
-            object : DrawerLayout.DrawerListener {
-                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-                    // Respond when the drawer's position changes
-                }
-                override fun onDrawerOpened(drawerView: View) {
-                    // Respond when the drawer is opened
-                }
-                override fun onDrawerClosed(drawerView: View) {
-                    // Respond when the drawer is closed
-                }
-                override fun onDrawerStateChanged(newState: Int) {
-                    // Respond when the drawer motion state changes
-                }
-            }
-        )*/
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                mDrawerLayout.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+
+
+    // Extension function to show toast message easily
+    private fun Context.toast(message:String){
+        Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
     }
 }
