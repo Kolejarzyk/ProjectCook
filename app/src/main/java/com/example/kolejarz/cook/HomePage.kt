f5fb7718d65a03bcffffdc8188862cb5cc3143c1
hentaininja <hasamt94@gmail.com>
2018-12-15 17:46:14 +0100

    the only commit i feel is suicide :(

--- app/src/main/java/com/example/kolejarz/cook/HomePage.kt
+++ app/src/main/java/com/example/kolejarz/cook/HomePage.kt
@@ -3,6 +3,8 @@ package com.example.kolejarz.cook
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_home_page.*
 
 class HomePage : AppCompatActivity() {
@@ -11,6 +13,14 @@ class HomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
 
         sing_in_button_home_page.setOnClickListener{
             val intent = Intent(this, LoginPage::class.java)
--- app/src/main/java/com/example/kolejarz/cook/SidePanelPage.kt
+++ app/src/main/java/com/example/kolejarz/cook/SidePanelPage.kt
@@ -2,37 +2,70 @@ package com.example.kolejarz.cook
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_sidepanel.*
 
 class SidePanelPage : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sidepanel)
 
 
-        create_recipe_button_side_panel.setOnClickListener{
-            val intent = Intent(this, LoginPage::class.java)
-            startActivity(intent)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
         }
 
-        my_builds_button_side_panel.setOnClickListener{
-            val intent = Intent(this, RegisterPage::class.java)
-            startActivity(intent)
-        }
-        favourites_button_side_panel.setOnClickListener{
-            val intent = Intent(this, RegisterPage::class.java)
-            startActivity(intent)
-        }
-        ranking_button_side_panel.setOnClickListener{
-            val intent = Intent(this, RegisterPage::class.java)
-            startActivity(intent)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            true
         }
 
-        products_list_button_side_panel.setOnClickListener{
-            val intent = Intent(this, RegisterPage::class.java)
-            startActivity(intent)
+        /*mDrawerLayout.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    // Respond when the drawer's position changes
+                }
+                override fun onDrawerOpened(drawerView: View) {
+                    // Respond when the drawer is opened
+                }
+                override fun onDrawerClosed(drawerView: View) {
+                    // Respond when the drawer is closed
+                }
+                override fun onDrawerStateChanged(newState: Int) {
+                    // Respond when the drawer motion state changes
+                }
+            }
+        )*/
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
