a94566caad6f267e6032701d41067e415988c053
Kolejarzyk <pawel.dee@live.com>
2018-12-15 19:29:57 +0100

    navigation drawer with fragments

--- app/src/main/java/com/example/kolejarz/cook/HomePage.kt
+++ app/src/main/java/com/example/kolejarz/cook/HomePage.kt
@@ -3,8 +3,6 @@ package com.example.kolejarz.cook
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.ActionBar
-import android.support.v7.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_home_page.*
 
 class HomePage : AppCompatActivity() {
@@ -13,14 +11,6 @@ class HomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        val actionbar: ActionBar? = supportActionBar
-        actionbar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
-        }
 
         sing_in_button_home_page.setOnClickListener{
             val intent = Intent(this, LoginPage::class.java)
@@ -34,5 +24,3 @@ class HomePage : AppCompatActivity() {
     }
 
 }
-
-
--- app/src/main/java/com/example/kolejarz/cook/LoginPage.kt
+++ app/src/main/java/com/example/kolejarz/cook/LoginPage.kt
@@ -1,7 +1,9 @@
 package com.example.kolejarz.cook
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login_page.*
 
 
 class LoginPage : AppCompatActivity()
@@ -9,5 +11,13 @@ class LoginPage : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        login_button_login.setOnClickListener{
+            val intent = Intent(this, UserPage::class.java)
+            startActivity(intent)
+
     }
+
+
+}
 }
